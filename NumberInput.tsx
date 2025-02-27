f35997bba6857baca89f560ce3d6bd3308fa4338
billy-r <lambdalocator.exe@gmail.com>
2025-02-27 12:01:50 +0000

    Update NumberInput.tsx

--- NumberInput.tsx
+++ NumberInput.tsx
@@ -30,26 +30,6 @@ export function NumberInput({ value, onChange, className }: NumberInputProps) {
         onChange={(e) => onChange(e.target.value)}
         className="pr-8 [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
       />
-      <div className="absolute right-1 top-1 bottom-1 flex flex-col gap-0.5">
-        <Button
-          type="button"
-          variant="ghost"
-          size="icon"
-          className="h-[calc(50%-2px)] w-6 p-0 opacity-50 hover:opacity-100 hover:bg-primary/10"
-          onClick={handleIncrement}
-        >
-          <ChevronUp className="h-3 w-3" />
-        </Button>
-        <Button
-          type="button"
-          variant="ghost"
-          size="icon"
-          className="h-[calc(50%-2px)] w-6 p-0 opacity-50 hover:opacity-100 hover:bg-primary/10"
-          onClick={handleDecrement}
-        >
-          <ChevronDown className="h-3 w-3" />
-        </Button>
-      </div>
     </div>
   );
 }
